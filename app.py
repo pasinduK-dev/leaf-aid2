739411dfe559a8562ec2b0def411f8745bc32cc6
pasinduK-dev <124087243+pasinduK-dev@users.noreply.github.com>
2025-02-16 04:40:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,3 @@ async def predict(file: UploadFile = File(...)):
         return JSONResponse(
             content={"error": f"An error occurred: {str(e)}"}, status_code=500
         )
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
